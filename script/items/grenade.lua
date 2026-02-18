f33748f1f203b1c2d767afb6986485cb2ad49c2d
PackMan09 <pacmanproductionsz25@gmail.com>
2026-02-18 13:36:36 -0700

    add formatting to gren to split world object and weapon

--- script/items/grenade.lua
+++ script/items/grenade.lua
@@ -6,7 +6,8 @@
 #include "script/util.lua"
 
 -- Per weapon constants
-local PRIM_FIRESOUND = "MOD/snd/249_fr0.ogg"
+local PRIM_FIRESOUND = "MOD/snd/gren.ogg"
+local BOUNCESOUND = "MOD/snd/grenBounce0"
 local PICKUP_SIZE = 5
 local RECOIL_AMNT = 0.075
 local FIRERATE = 0.5
@@ -15,144 +16,152 @@ local FUZESTART = 3.0
 local WPNID = "hlgrenade"
 local WPNNAME = "M1 Frag"
 
--- Per weapon data storer
-FRAGplayers = {}
+do -- GRENADE WEAPON
 
-function createPlayerDataFRAG()
-    return {
-		coolDown = 0.0,
-		recoil = 0.0,
-		toolAnimator = ToolAnimator(),
-		firesound = nil,	
-	}
-end
+	-- Per weapon data storer
+	FRAGplayers = {}
 
-function server.initFRAG()
-	RegisterTool(WPNID, WPNNAME, "MOD/prefab/grenade.xml", 5)
-	SetToolAmmoPickupAmount(WPNID, PICKUP_SIZE)
-end
-
-function server.tickFRAG(dt)
-	for p in PlayersAdded() do
-		FRAGplayers[p] = createPlayerDataFRAG()
-		SetToolEnabled(WPNID, true, p)
-		SetToolAmmo(WPNID, 250, p)
+	function createPlayerDataFRAG()
+		return {
+			coolDown = 0.0,
+			recoil = 0.0,
+			toolAnimator = ToolAnimator(),
+			firesound = nil,	
+		}
 	end
 
-	for p in PlayersRemoved() do
-		FRAGplayers[p] = nil
+	function server.initFRAG()
+		RegisterTool(WPNID, WPNNAME, "MOD/prefab/grenade.xml", 5)
+		SetToolAmmoPickupAmount(WPNID, PICKUP_SIZE)
 	end
 
-	for p in Players() do
-		server.tickPlayerFRAG(p, dt)
-	end
-end
+	function server.tickFRAG(dt)
+		for p in PlayersAdded() do
+			FRAGplayers[p] = createPlayerDataFRAG()
+			SetToolEnabled(WPNID, true, p)
+			SetToolAmmo(WPNID, 250, p)
+		end
 
-function server.tickPlayerFRAG(p, dt)
-	if GetPlayerHealth(p) <= 0 then
-		FRAGplayers[p] = createPlayerDataFRAG()
-		return
-	end
-end
+		for p in PlayersRemoved() do
+			FRAGplayers[p] = nil
+		end
 
-function server.primaryFireFRAG(p)
-	local mt = GetToolLocationWorldTransform("muzzle", p)
-
-	local ammo = GetToolAmmo(WPNID, p)
-	local data = FRAGplayers[p]
-
-	local _,pos,_,dir = GetPlayerAimInfo(mt.pos, MAX_RANGE, p)
-	local pvel = GetPlayerVelocity(p)
-
-	--ShootHook(pos, dir, "bullet", DAMAGE, PLAYERDAMAGE, MAX_RANGE, p, WPNID, WPNNAME)
-	
-	StopSound(data.firesound)
-	data.firesound = PlaySound(LoadSound(PRIM_FIRESOUND), mt.pos, 300)
-
-	if ammo < 9999 then
-		SetToolAmmo(WPNID, ammo-1, p)
-	end
-end
-
-function client.initFRAG()
-	shootHaptic = LoadHaptic("MOD/haptic/gun_fire.xml")
-	local toolHaptic = LoadHaptic("MOD/haptic/background.xml")
-	SetToolHaptic(WPNID, toolHaptic)
-end
-
-function client.tickFRAG(dt)
-	for p in PlayersAdded() do
-		FRAGplayers[p] = createPlayerDataFRAG();
+		for p in Players() do
+			server.tickPlayerFRAG(p, dt)
+		end
 	end
 
-	for p in PlayersRemoved() do
-		FRAGplayers[p] = nil
+	function server.tickPlayerFRAG(p, dt)
+		if GetPlayerHealth(p) <= 0 then
+			FRAGplayers[p] = createPlayerDataFRAG()
+			return
+		end
 	end
 
-	for p in Players() do
-		client.tickPlayerFRAG(p, dt)
-	end
-end
+	function server.primaryFireFRAG(p)
+		local mt = GetToolLocationWorldTransform("muzzle", p)
 
-function client.tickPlayerFRAG(p, dt)
-	if GetPlayerHealth(p) <= 0 then
-		FRAGplayers[p] = createPlayerDataFRAG()
-		return
-	end
-	
-	if GetPlayerTool(p) ~= WPNID then
-		return
+		local ammo = GetToolAmmo(WPNID, p)
+		local data = FRAGplayers[p]
+
+		local _,pos,_,dir = GetPlayerAimInfo(mt.pos, MAX_RANGE, p)
+		local pvel = GetPlayerVelocity(p)
+
+		--ShootHook(pos, dir, "bullet", DAMAGE, PLAYERDAMAGE, MAX_RANGE, p, WPNID, WPNNAME)
+		
+		StopSound(data.firesound)
+		data.firesound = PlaySound(LoadSound(PRIM_FIRESOUND), mt.pos, 300)
+
+		if ammo < 9999 then
+			SetToolAmmo(WPNID, ammo-1, p)
+		end
 	end
 
-	local ammo = GetToolAmmo(WPNID, p)
-	
-	local data = FRAGplayers[p]
+	function client.initFRAG()
+		shootHaptic = LoadHaptic("MOD/haptic/gun_fire.xml")
+		local toolHaptic = LoadHaptic("MOD/haptic/background.xml")
+		SetToolHaptic(WPNID, toolHaptic)
+	end
 
-	if InputDown("usetool", p) and ammo > 0.5 and GetPlayerCanUseTool(p) == true then
-			if data.coolDown < 0 then
-				if IsPlayerLocal(p) then
-					ServerCall("server.primaryFireFRAG", p)
+	function client.tickFRAG(dt)
+		for p in PlayersAdded() do
+			FRAGplayers[p] = createPlayerDataFRAG();
+		end
+
+		for p in PlayersRemoved() do
+			FRAGplayers[p] = nil
+		end
+
+		for p in Players() do
+			client.tickPlayerFRAG(p, dt)
+		end
+	end
+
+	function client.tickPlayerFRAG(p, dt)
+		if GetPlayerHealth(p) <= 0 then
+			FRAGplayers[p] = createPlayerDataFRAG()
+			return
+		end
+		
+		if GetPlayerTool(p) ~= WPNID then
+			return
+		end
+
+		local ammo = GetToolAmmo(WPNID, p)
+		
+		local data = FRAGplayers[p]
+
+		if InputDown("usetool", p) and ammo > 0.5 and GetPlayerCanUseTool(p) == true then
+				if data.coolDown < 0 then
+					if IsPlayerLocal(p) then
+						ServerCall("server.primaryFireFRAG", p)
+					end
+
+					data.coolDown = FIRERATE
+					
+					data.recoil = RECOIL_AMNT
 				end
 
-				data.coolDown = FIRERATE
-				
-				data.recoil = RECOIL_AMNT
+			if IsPlayerLocal(p) then
+				PlayHaptic(shootHaptic, 1)
+			end
+		end
+
+		-- decrease firing cooldown and recoil
+		data.coolDown = data.coolDown - dt
+		data.recoil = data.recoil - dt
+		
+		-- RECOIL
+		if data.recoil > -0.5 then
+			local recoil = math.max(0, data.recoil)
+			local siderecoil = recoil * 0.25
+			local recoilvert = math.max(0, data.recoil)
+			
+			local inversesiderecoil = rnd(0, 1)
+			if inversesiderecoil > 0.5 then
+				siderecoil = siderecoil * -1
 			end
 
-		if IsPlayerLocal(p) then
-			PlayHaptic(shootHaptic, 1)
-		end
-	end
-
-	-- decrease firing cooldown and recoil
-	data.coolDown = data.coolDown - dt
-	data.recoil = data.recoil - dt
-	
-	-- RECOIL
-	if data.recoil > -0.5 then
-		local recoil = math.max(0, data.recoil)
-		local siderecoil = recoil * 0.25
-		local recoilvert = math.max(0, data.recoil)
+			data.toolAnimator.offsetTransform = Transform(Vec(siderecoil,recoil,recoilvert))
+		end 
+		-- END RECOIL
 		
-		local inversesiderecoil = rnd(0, 1)
-		if inversesiderecoil > 0.5 then
-			siderecoil = siderecoil * -1
-		end
+		local toolBody = GetToolBody(p)
+		if toolBody ~= 0 then -- hide shells if low ammo
+			local shapes = GetBodyShapes(toolBody)
 
-		data.toolAnimator.offsetTransform = Transform(Vec(siderecoil,recoil,recoilvert))
-	end 
-	-- END RECOIL
-	
-	local toolBody = GetToolBody(p)
-	if toolBody ~= 0 then -- hide shells if low ammo
-		local shapes = GetBodyShapes(toolBody)
-
-		if ammo < 0.5 then -- no grenades
-			-- hide grenade
-			SetTag(shapes[0], "invisible")
-		elseif HasTag(shapes[0], "invisible") == true then
-			RemoveTag(shapes[0], "invisible")
+			if ammo < 0.5 then -- no grenades
+				-- hide grenade
+				SetTag(shapes[0], "invisible")
+			elseif HasTag(shapes[0], "invisible") == true then
+				RemoveTag(shapes[0], "invisible")
+			end
 		end
+		tickToolAnimator(data.toolAnimator, dt, nil, p)
 	end
-	tickToolAnimator(data.toolAnimator, dt, nil, p)
+
+end
+
+do -- GRENADE WORLD
+	
 end
